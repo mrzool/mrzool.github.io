5e0093b6b388ce7ebe16f1a039a7d4a0149ba2db
zool <zool.bib@gmail.com>
2015-01-06 20:15:16 +0100

    Add new plugins, create task for prefixing and minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
+var gulp          = require('gulp');
+var browserSync   = require('browser-sync');
+var autoprefixer  = require('gulp-autoprefixer');
+var minifycss     = require('gulp-minify-css');
+var rename        = require('gulp-rename');
 
 gulp.task('browser-sync', function() {
     browserSync({
@@ -13,7 +16,15 @@ gulp.task('bs-reload', function() {
     browserSync.reload();
 });
 
-gulp.task('default', ['browser-sync'], function() {
+gulp.task('styles', function() {
+  return gulp.src('css/skeleton.css')
+    .pipe(rename({suffix: '.min'}))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(minifycss())
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('default', ['styles', 'browser-sync'], function() {
   gulp.watch("./_site/*.html", ['bs-reload']);
   gulp.watch("./_site/css/*.css", ['bs-reload']);
 });

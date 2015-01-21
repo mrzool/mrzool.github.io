0e362a3a5ff64fda54f6b8807b84533080a2070c
zool <zool.bib@gmail.com>
2015-01-21 15:21:58 +0100

    Change CSS path

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task('styles', function() {
   .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
   .pipe(rename({suffix: '.min'}))
   .pipe(minifycss())
-  .pipe(gulp.dest('css/prod'));
+  .pipe(gulp.dest('css'));
 });
 
 gulp.task('default', ['styles', 'browser-sync'], function() {

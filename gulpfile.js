ca051556fb697a73cb63b00729e6007e33342dc9
zool <zool.bib@gmail.com>
2015-01-25 11:51:57 +0100

    Set gulp to output the CSS in a dedicated directory

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('styles', function() {
   .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
   .pipe(rename({suffix: '.min'}))
   .pipe(minifycss())
-  .pipe(gulp.dest('css'));
+  .pipe(gulp.dest('css/prod'));
 });
 
 gulp.task('default', ['browser-sync'], function() {
